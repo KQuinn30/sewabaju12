8ce1917ec04ed33b3e6fde2b4c66c6765c292346
YodaPratamaPutra <yodapratama30@gmail.com>
2026-04-07 18:06:18 +0700

    tugas peweb

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
+Selamat Datang di Toko Sewa Baju Terbaik Di Indonesia!!! {{$nama}}<br>
+Anda adalah seorang {{$pekerjaan}}
+</div>
