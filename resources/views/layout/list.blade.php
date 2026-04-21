643d71813962e19615fd19a94e0021a0dae792a8
KQuinn30 <yodapratama30@gmail.com>
2026-04-21 11:53:40 +0700

    kiw kiw
    tambah 2 file

---
+++ resources/views/layout/list.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>@yield('title', 'My App')</title>
+</head>
+<body>
+    <h1>List Product</h1>
+    <div> class="container">
+        <main>
+            @yield('content')
+        </main>
+
+    </div>
+</body>
+</html>
